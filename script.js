b196d094a4356a9ccd99c32a7c9bb0b700427a2e
Ishan Saini <ishansaini0105@gmail.com>
2024-09-01 17:43:35 +0530

    Logic of the following website is written on the basis of a single logic of timezone and localtimestring

--- script.js
+++ script.js
@@ -7,3 +7,57 @@ usa.innerHTML=d.toLocaleTimeString('en-US', {
   });
 
 },1000)
+
+
+const london= document.getElementById('UK');
+setInterval(()=>{
+let d= new Date()
+london.innerHTML=d.toLocaleTimeString('en-US', {
+    timeZone: 'Europe/London', // or 'America/Los_Angeles' for Pacific Time
+  });
+
+},1000)
+
+
+const Japan= document.getElementById('Japan');
+setInterval(()=>{
+let d= new Date()
+Japan.innerHTML=d.toLocaleTimeString('en-US', {
+    timeZone: 'Asia/Tokyo', // or 'America/Los_Angeles' for Pacific Time
+  });
+
+},1000)
+
+
+
+
+const India= document.getElementById('India');
+setInterval(()=>{
+let d= new Date()
+India.innerHTML=d.toLocaleTimeString();
+
+},1000)
+
+
+
+
+const China= document.getElementById('China');
+setInterval(()=>{
+let d= new Date()
+China.innerHTML=d.toLocaleTimeString('en-US', {
+    timeZone: 'Asia/Shanghai', // or 'America/Los_Angeles' for Pacific Time
+  });
+
+},1000)
+
+
+
+
+const Russia= document.getElementById('Russia');
+setInterval(()=>{
+let d= new Date()
+Russia.innerHTML=d.toLocaleTimeString('en-US', {
+    timeZone: 'Europe/Moscow', // or 'America/Los_Angeles' for Pacific Time
+  });
+
+},1000)
